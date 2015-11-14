a751c113ad97143cb5e3b40874b703a29b0c3f27
Jens W. Klein <jk@kleinundpartner.at>
2015-11-14 01:02:39 +0100

    minimal norecaptche version is 0.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'norecaptcha',
+        'norecaptcha>=0.3',
         'plone.app.registry',
         'plone.registry',
         'Products.CMFCore',
