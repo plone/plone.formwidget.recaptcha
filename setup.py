f1bf15b993676f9a6a48d4f39f132927b8951179
Jens W. Klein <jk@kleinundpartner.at>
2019-02-20 11:32:12 +0100

    adjust version and add classifiers for plone 5 on py2.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.0.1.dev0'
+version = '2.1.dev0'
 
 description = open("README.rst").read() + "\n"
 description += open("CHANGES.rst").read()
@@ -17,8 +17,9 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
-        # "Framework :: Plone :: 5.0",
-        # "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
