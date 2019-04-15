b05ea08d70df0f7257062b7ab8eba0dfde45cc6b
Peter Mathis <peter.mathis@kombinat.at>
2019-04-15 10:53:18 +0200

    disable code-analysis part and add basic setup test

---
+++ src/plone/formwidget/recaptcha/testing.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import plone.formwidget.recaptcha
+
+
+class PloneFormwidgetRecaptchaLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        self.loadZCML(package=plone.formwidget.recaptcha)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'plone.formwidget.recaptcha:default')
+
+
+PLONE_FORMWIDGET_RECAPTCHA_FIXTURE = PloneFormwidgetRecaptchaLayer()
+
+
+PLONE_FORMWIDGET_RECAPTCHA_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONE_FORMWIDGET_RECAPTCHA_FIXTURE,),
+    name='PloneFormwidgetRecaptchaLayer:IntegrationTesting',
+)
+
+
+PLONE_FORMWIDGET_RECAPTCHA_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(PLONE_FORMWIDGET_RECAPTCHA_FIXTURE,),
+    name='PloneFormwidgetRecaptchaLayer:FunctionalTesting',
+)
+
+
+PLONE_FORMWIDGET_RECAPTCHA_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        PLONE_FORMWIDGET_RECAPTCHA_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE,
+    ),
+    name='PloneFormwidgetRecaptchaLayer:AcceptanceTesting',
+)
---
+++ src/plone/formwidget/recaptcha/tests/test_setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from plone.formwidget.recaptcha.testing import PLONE_FORMWIDGET_RECAPTCHA_INTEGRATION_TESTING  # noqa: E501
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+
+import unittest
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
+
+class TestSetup(unittest.TestCase):
+    """Test that plone.formwidget.recaptcha is properly installed."""
+
+    layer = PLONE_FORMWIDGET_RECAPTCHA_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_product_installed(self):
+        """Test if plone.formwidget.recaptcha is installed."""
+        self.assertTrue(self.installer.isProductInstalled(
+            'plone.formwidget.recaptcha'))
+
+    def test_browserlayer(self):
+        """Test that IReCaptchaLayer is registered."""
+        from plone.formwidget.recaptcha.interfaces import IReCaptchaLayer
+        from plone.browserlayer import utils
+        self.assertIn(
+            IReCaptchaLayer,
+            utils.registered_layers())
+
+
+class TestUninstall(unittest.TestCase):
+
+    layer = PLONE_FORMWIDGET_RECAPTCHA_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+        roles_before = api.user.get_roles(TEST_USER_ID)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.installer.uninstallProducts(['plone.formwidget.recaptcha'])
+        setRoles(self.portal, TEST_USER_ID, roles_before)
+
+    def test_product_uninstalled(self):
+        """Test if plone.formwidget.recaptcha is cleanly uninstalled."""
+        self.assertFalse(self.installer.isProductInstalled(
+            'plone.formwidget.recaptcha'))
+
+    def test_browserlayer_removed(self):
+        """Test that IReCaptchaLayer is removed."""
+        from plone.formwidget.recaptcha.interfaces import IReCaptchaLayer
+        from plone.browserlayer import utils
+        self.assertNotIn(
+            IReCaptchaLayer,
+            utils.registered_layers())
