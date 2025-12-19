b829d38b5c05e0b126599428ede1a759131f9220
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-12-19 23:45:04 +0100

    update supported python versions

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.9,<=3.13",
+    python_requires=">=3.9,<3.14",
     install_requires=[
         "Acquisition",
         "plone.app.registry",
