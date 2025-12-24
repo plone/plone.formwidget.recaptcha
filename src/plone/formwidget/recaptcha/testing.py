ec810cf9b6f362e81ce498683046999af99c68f7
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-12-24 16:18:59 +0100

    isort

--- src/plone/formwidget/recaptcha/testing.py
+++ src/plone/formwidget/recaptcha/testing.py
@@ -1,9 +1,10 @@
 from plone.app.testing import applyProfile
-from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.testing import zope
+
 import plone.formwidget.recaptcha
 
 
